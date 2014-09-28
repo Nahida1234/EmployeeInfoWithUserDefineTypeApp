c56230363411c476adce91dc0681c2bce02cb8fb
nahida1234 <nahida_036@yahoo.com>
2014-09-28 03:01:37 -0400

    Employee Info With User Define Type

---
+++ EmployeeInfoFormUI.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace EmployeeInfoWithUserDefineTypeApp
+{
+    partial class EmployeeInfoFormUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.retriveButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(95, 42);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 0;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(95, 91);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(95, 138);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.salaryTextBox.TabIndex = 0;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(36, 203);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(81, 27);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // retriveButton
+            // 
+            this.retriveButton.Location = new System.Drawing.Point(142, 203);
+            this.retriveButton.Name = "retriveButton";
+            this.retriveButton.Size = new System.Drawing.Size(81, 27);
+            this.retriveButton.TabIndex = 1;
+            this.retriveButton.Text = "Retrive";
+            this.retriveButton.UseVisualStyleBackColor = true;
+            this.retriveButton.Click += new System.EventHandler(this.retriveButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Salary";
+            // 
+            // EmployeeInfoFormUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.retriveButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Name = "EmployeeInfoFormUI";
+            this.Text = "Employee Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button retriveButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EmployeeInfoFormUI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeInfoWithUserDefineTypeApp
+{
+    public partial class EmployeeInfoFormUI : Form
+    {
+        public EmployeeInfoFormUI()
+        {
+            InitializeComponent();
+        }
+
+   Employee anEmployee=new Employee();
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+            anEmployee.id = idTextBox.Text;
+            anEmployee.name = nameTextBox.Text;
+            anEmployee.salary = Convert.ToDouble(salaryTextBox.Text);
+                MessageBox.Show(@"Information Saved Successfully");
+
+                idTextBox.Text = "";
+                nameTextBox.Text = "";
+                salaryTextBox.Text = "";
+          
+
+        }
+
+        private void retriveButton_Click(object sender, EventArgs e)
+        {
+            idTextBox.Text = anEmployee.id;
+            nameTextBox.Text = anEmployee.name;
+            salaryTextBox.Text = anEmployee.salary.ToString();
+            MessageBox.Show(@"Information Retrive Successfully");
+
+        }
+
+      
+
+    }
+}
